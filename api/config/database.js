2726434aa2305608ec4a2607609c871b22b0ba73
Henrique Albuquerque <henriquemoreira842@gmail.com>
2024-07-03 17:29:10 -0300

    Update database.js

--- api/config/database.js
+++ api/config/database.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
-const sequelize = new Sequelize('database_development', 'root', '', {
+const sequelize = new Sequelize('byev3uoiifqc8mqiqnsl', 'uexfcvn1ahfatxc0', 'qwrnWt2gKDjVh7lCY1L9', {
     dialect: 'mysql',
-    host: 'localhost',
+    host: 'byev3uoiifqc8mqiqnsl-mysql.services.clever-cloud.com',
     port: '3306'
 })
 
